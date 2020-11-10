941871586ceee7ddfee86d7ed3b3a82412ecbe40
Mohammed Naser <mnaser@vexxhost.com>
2020-11-09 19:09:41 -0500

    Added variables description

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "region" {
-  type = string
+  type        = string
+  description = "Name of the region to upload images to"
 }
 
 variable "architectures" {
-  type    = list
+  type = list
+  name = "List of supported architectures by cloud"
 }
